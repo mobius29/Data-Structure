2a4306842378686ca61eff154d771e630eadf62d
mobius29 <sagjin0000@naver.com>
2023-08-16 12:04:53 +0900

    feat: implement stack data structure

---
+++ rust/stack.rs
@@ -0,0 +1,81 @@
+struct Stack<T> {
+  head: NextNode<T>,
+  size: usize,
+}
+
+struct Node<T> {
+  value: T,
+  next_node: NextNode<T>,
+}
+
+type NextNode<T> = Option<Box<Node<T>>>;
+
+impl<T> Stack<T> {
+  pub fn new() -> Self {
+    Self {
+      head: None,
+      size: 0,
+    }
+  }
+
+  pub fn push(&mut self, value: T) {
+    let new_node = Box::new(Node {
+      value,
+      next_node: self.head.take(),
+    });
+
+    self.size += 1;
+    self.head = Some(new_node);
+  }
+
+  pub fn pop(&mut self) -> Result<(), &str> {
+    match self.head.take() {
+      Some(node) => {
+        self.head = node.next_node;
+        self.size -= 1;
+
+        Ok(())
+      },
+      None => Err("Cannot pop this stack because stack is empty."),
+    }
+  }
+
+  pub fn top(&mut self) -> Result<&T, &str> {
+    match self.head.as_ref() {
+      Some(node) => Ok(&node.value),
+      None => Err("Cannot get value of this stack because stack is empty."),
+    }
+  }
+
+  pub fn top_mut(&mut self) -> Result<&mut T, &str> {
+    match self.head.as_mut() {
+      Some(node) => Ok(&mut node.value),
+      None => Err("Cannot get value of this stack because stack is empty."),
+    }
+  }
+
+  pub fn empty(&self) -> bool {
+    self.head.is_none()
+  }
+
+  pub fn clear(&mut self) -> () {
+    while !self.empty() {
+      self.pop().unwrap();
+    }
+  }
+}
+
+fn main() {
+  let mut stack: Stack<i32> = Stack::new();
+
+  for i in 0..=9 {
+      stack.push(i);
+  }
+
+  while !stack.empty() {
+      let top = *stack.top().unwrap();
+      stack.pop().unwrap();
+
+      println!("{}", top);
+  }
+}
